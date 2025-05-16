a4e7e469a2b687306c4b2fdd592b1763f3eaf215
endermaru <endermaru>
2025-05-17 02:01:18 +0900

    mofied new lines

--- Testset/joi_extraction.py
+++ Testset/joi_extraction.py
@@ -11,6 +11,14 @@ def literal_str_representer(dumper, data):
 
 yaml.add_representer(LiteralString, literal_str_representer)
 
+class QuotedString(str):
+    pass
+
+def quoted_str_representer(dumper, data):
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+
+yaml.add_representer(QuotedString, quoted_str_representer)
+
 def parse_scenarios_with_command(script: str):
     lines = script.strip().splitlines()
     command = lines[0].replace("\",,","").replace("\"","")
@@ -59,19 +67,45 @@ def parse_scenarios(script: str):
       "code": scenarios
     }
 
-dsl_code = '''
-에어컨이 지원하는 모드를 스피커로 출력해줘
-name = "Scenario1"
-cron = ""
-period = -1
-modes_str = (#AirConditioner).airConditionerMode_supportedAcModes
-(#Speaker).mediaPlayback_speak(modes_str)
-'''
+# dsl_code = '''
+# 에어컨이 지원하는 모드를 스피커로 출력해줘
+# name = "Scenario1"
+# cron = ""
+# period = -1
+# modes_str = (#AirConditioner).airConditionerMode_supportedAcModes
+# (#Speaker).mediaPlayback_speak(modes_str)
+# '''
 
-# print(json.dumps(parse_scenarios(dsl_code), indent=2))
-with open("./test.yaml", "w", encoding="utf-8") as f:
-    yaml.dump([parse_scenarios_with_command(dsl_code)], f, indent=2, allow_unicode=True, sort_keys=False)
-# print(yaml.dump(parse_scenarios(dsl_code), indent=2, allow_unicode=True, sort_keys=False))
+# # print(json.dumps(parse_scenarios(dsl_code), indent=2))
+# with open("./test.yaml", "w", encoding="utf-8") as f:
+#     yaml.dump([parse_scenarios_with_command(dsl_code)], f, indent=2, allow_unicode=True, sort_keys=False)
+# # print(yaml.dump(parse_scenarios(dsl_code), indent=2, allow_unicode=True, sort_keys=False))
+
+# # 줄바꿈 변환
+# def process_fields(data, quote_fields, literal_fields):
+#     if isinstance(data, list):
+#         for item in data:
+#             process_fields(item, quote_fields, literal_fields)
+#     elif isinstance(data, dict):
+#         for key, value in data.items():
+#             if key in quote_fields and isinstance(value, str):
+#                 # 줄바꿈 제거 및 큰따옴표로 감싸기
+#                 single_line = re.sub(r'\s*\n\s*', ' ', value).strip()
+#                 data[key] = QuotedString(single_line)
+#             elif key in literal_fields and isinstance(value, str):
+#                 # LiteralString으로 변환
+#                 data[key] = LiteralString(value)
+#             else:
+#                 process_fields(value, quote_fields, literal_fields)
+
+
+# for file in os.listdir("./Testset"):
+#   if file.endswith("yaml"):
+#     with open(f"./Testset/{file}", "r", encoding="utf-8") as f:
+#       yaml_data = yaml.safe_load(f)
+#     process_fields(yaml_data, quote_fields=["command", "name", "cron"], literal_fields=["code"])
+#     with open(f"./Testset/{file}", "w", encoding="utf-8") as f:
+#       yaml.dump(yaml_data, f, allow_unicode=True, sort_keys=False, default_flow_style=False, width=float('inf'))
 
 # # 예전 json을 yaml로 변환
 # def adjust_indentation(code_str, from_spaces=4, to_spaces=2):
