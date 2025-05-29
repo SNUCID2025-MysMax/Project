7a17aedf68cae21c4a13e53f5daa2e117ed2d39e
Ubuntu <ubuntu@ip-172-31-13-181.ap-northeast-2.compute.internal>
2025-05-29 14:32:01 +0000

    reflect alarms

--- Testset/add_devices.py
+++ Testset/add_devices.py
@@ -35,40 +35,40 @@ exclude = {"Wall", "SectorA", "SectorB", "Upper", "Lower", "Even", "Odd"}
 
 
 
-# for i in range(0,16):
-#     file_name = f"category_{i}.yaml"
-#     print(f"Processing file: {file_name}")
-#     path_in = f"./Testset/Testset/{file_name}"
-#     path_out = f"./Testset/TestsetWithDevices/{file_name}"
+for i in range(0,16):
+    file_name = f"category_{i}.yaml"
+    print(f"Processing file: {file_name}")
+    path_in = f"./Testset/Testset/{file_name}"
+    path_out = f"./Testset/TestsetWithDevices/{file_name}"
 
-#     with open(path_in, "r") as f:
-#         data = yaml.safe_load(f)
+    with open(path_in, "r") as f:
+        data = yaml.safe_load(f)
 
-#     # 각 항목에 devices 키 추가
-#     for item in tqdm.tqdm(data):
-#         user_command = item["command"]
+    # 각 항목에 devices 키 추가
+    for item in tqdm.tqdm(data):
+        user_command = item["command"]
 
-#         result = set()
+        result = set()
 
-#         for code in item["code"]:
-#             code["code"] = LiteralString(code["code"])
+        for code in item["code"]:
+            code["code"] = LiteralString(code["code"])
 
-#             text = code["code"]
-#             matches = re.findall(r"#([A-Za-z0-9]+)(?=[^A-Za-z0-9])", text)
-#             matches = {m for m in matches if m not in exclude}
-#             result.update(matches)
+            text = code["code"]
+            matches = re.findall(r"#([A-Za-z0-9]+)(?=[^A-Za-z0-9])", text)
+            matches = {m for m in matches if m not in exclude}
+            result.update(matches)
         
-#         item["devices"] = list(result)
+        item["devices"] = list(result)
 
 
 
-#         # 만약 첫 번째 항목만 처리 후 멈추려면 이 줄을 유지하세요
-#         # break
+        # 만약 첫 번째 항목만 처리 후 멈추려면 이 줄을 유지하세요
+        # break
 
 
-#     # 변경된 데이터 YAML로 저장
-#     with open(path_out, "w") as f:
-#         yaml.safe_dump(data, f, allow_unicode=True, sort_keys=False,  width=float('inf'), default_flow_style=False)
+    # 변경된 데이터 YAML로 저장
+    with open(path_out, "w") as f:
+        yaml.safe_dump(data, f, allow_unicode=True, sort_keys=False,  width=float('inf'), default_flow_style=False)
 
 #############################################33
 import re
